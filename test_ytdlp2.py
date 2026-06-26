a1d7058e4d09e869b39d7588ca7611cf147df3a3
Axi∅n-dev <adarashshukla7@gmail.com>
2026-06-26 23:50:05 +0530

    00 issue solve

---
+++ test_ytdlp2.py
@@ -0,0 +1,16 @@
+import yt_dlp
+
+ydl_opts = {
+    'format': 'bestaudio',
+    'quiet': True,
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['mweb', 'web', 'tv'],
+        }
+    },
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info('https://music.youtube.com/watch?v=dQw4w9WgXcQ', download=False)
+    print("URL FOUND:", info.get('url') is not None)
+    print("URL START:", info['url'][:50] if 'url' in info else "NONE")
---
+++ test_ytdlp3.py
@@ -0,0 +1,11 @@
+import yt_dlp
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'quiet': True,
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    info = ydl.extract_info('https://music.youtube.com/watch?v=dQw4w9WgXcQ', download=False)
+    print("URL FOUND:", info.get('url') is not None)
+    print("URL START:", info['url'][:50] if 'url' in info else "NONE")
